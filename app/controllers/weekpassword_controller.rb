2c40f24b84877f078fbb4b4d9466200aba974108
Jhon <john@ezjie.com>
2016-07-06 12:03:50 +0800

    增加弱密码检测

--- app/controllers/weekpassword_controller.rb
+++ app/controllers/weekpassword_controller.rb
@@ -17,9 +17,12 @@ class WeekpasswordController < ApplicationController
 
     result = cracker.check_is_week password
 
+    if !result
+      result =  '密码看上去是强壮的,因为从我们现有的字典里没发现这样的密码.'
+    else
 
-    result =  '密码看上去是强壮的,因为从我们现有的字典里没发现这样的密码.' if !result
-    result =  "密码属于弱密码,因为在字典<#{result}>中被发现" if result
+      result =  "密码属于弱密码,因为在字典<#{result}>中被发现"
+    end
 
     render :json=>{'result'=>result}
   end
